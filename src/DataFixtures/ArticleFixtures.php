21363f76e0a02929b4402f16da7fabe8ffd22995
antoinich <antoinejen@hotmail.com>
2020-04-21 10:03:44 +0200

    adding security package

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,21 +6,22 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\Tag;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
-
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
         'Life on Planet Mercury: Tan, Relaxing and Fabulous',
         'Light Speed Travel: Fountain of Youth or Fallacy',
     ];
+
     private static $articleImages = [
         'asteroid.jpeg',
         'mercury.jpeg',
         'lightspeed.png',
     ];
+
     private static $articleAuthors = [
         'Mike Ferengi',
         'Amy Oort',
@@ -28,49 +29,48 @@ class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 
     protected function loadData(ObjectManager $manager)
     {
+        $this->createMany(10, 'main_articles', function($count) use ($manager) {
+            $article = new Article();
+            $article->setTitle($this->faker->randomElement(self::$articleTitles))
+                ->setContent(<<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
 
-        $this->createMany(Article::class, 10, function ($article, $count) use ($manager) {
-
-        $article->setTitle($this->faker->randomElement(self::$articleTitles))
-            ->setContent(<<<EOF
-Laboris beef ribs fatback fugiat eiusmod jowl **kielbasa** alcatra dolore velit ea ball tip. Pariatur
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
-capicola biltong [frankfurter boudin] (https://baconipsum.com/) cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
 picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
 occaecat lorem meatball prosciutto quis strip steak.
-                        
-**Meatball** adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
 mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
-Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin buffalo minim ex quis. Pork belly
-pork chop officia anim. Irure tempor leberkas kevin adipisicing cupidatat qui buffalo ham aliqua pork belly
-exercitation eiusmod. Exercitation incididunt rump laborum, t-bone short ribs buffalo ut shankle pork chop
-bresaola shoulder burgdoggen fugiat. Adipisicing nostrud chicken consequat beef ribs, quis filet mignon do.
-Prosciutto capicola mollit shankle aliquip do dolore hamburger brisket turducken eu.
 EOF
-            )
-        ;
+                );
 
-        if ($this->faker->boolean(70)) {
-            $article->setPublishedAt($this->faker->dateTimeBetween('-100 days' , '-1 days'));
-        }
+            // publish most articles
+            if ($this->faker->boolean(70)) {
+                $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
+            }
 
-        $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
-            ->setHeartCount($this->faker->numberBetween(5, 100))
-            ->setImageFilename($this->faker->randomElement(self::$articleImages));
+            $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
+                ->setHeartCount($this->faker->numberBetween(5, 100))
+                ->setImageFilename($this->faker->randomElement(self::$articleImages))
+            ;
 
-        /**@var Tag[] $tags */
-        $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
-        foreach ($tags as $tag) {
-            $article->addTag($tag);
-        }
+            $tags = $this->getRandomReferences('main_tags', $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+            }
 
+            return $article;
+        });
 
-    });
         $manager->flush();
-
     }
 
     public function getDependencies()
@@ -79,5 +79,4 @@ EOF
             TagFixture::class,
         ];
     }
-
 }
--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -3,33 +3,34 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Comment::class, 100, function (Comment $comment) {
+        $this->createMany(100, 'main_comments', function() {
+            $comment = new Comment();
             $comment->setContent(
-                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentence(2, true)
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
             );
-            $comment->setAuthorName($this->faker->name);
-            $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds' ));
-            $comment->setIsDeleted($this->faker->boolean(20));
 
-            $comment->setArticle($this->getRandomReference(Article::class));
+            $comment->setAuthorName($this->faker->name);
+            $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
+            $comment->setIsDeleted($this->faker->boolean(20));
+            $comment->setArticle($this->getRandomReference('main_articles'));
+
+            return $comment;
         });
+
         $manager->flush();
     }
 
     public function getDependencies()
     {
-        return [
-            ArticleFixtures::class,
-        ];
+        return [ArticleFixtures::class];
     }
-
-
 }
+
