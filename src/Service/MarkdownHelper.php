da615f8fa67dd840c1575d3e592320d3ee9410fe
antoinich <antoinejen@hotmail.com>
2020-04-28 10:29:29 +0200

    adding auth and creating api auth

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -7,6 +7,7 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper
 {
@@ -19,19 +20,24 @@ class MarkdownHelper
 
     private $isDebug;
 
+    private $security;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug, Security $security)
     {
         $this->cache= $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
         $this->isDebug = $isDebug;
+        $this->security = $security;
     }
 
     public function parse(string $source): string
     {
         if (stripos($source, 'bacon') !== false) {
-            $this->logger->info('They are are talking about bacon again!');
+            $this->logger->info('They are are talking about bacon again!', [
+                'user' => $this->security->getUser(),
+            ]);
         }
 
         if ($this->isDebug) {
